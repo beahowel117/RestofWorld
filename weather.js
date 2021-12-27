56a649b7809da74109755506dbe9d83eb25fafc2
Beth Howell <beahowel117@gmail.com>
2021-12-26 22:05:02 -0600

    Part 2 finished

--- weather.js
+++ weather.js
@@ -1,31 +1,9 @@
-/**
- *
-
-         <script>
-     function fetchWeather(data){
-       let text = '';
-       let len = data.length;
-       for(let i = 0; i < len; i++){
-         cities = data[i];
-         text += '<p><img src= "'
-       }
-       document.getElementById('cities').innerHTML = text;
-     }
-
-  </script>
- */
-// let weather = {
-//   fetchWeather: function(){
-//     fetch("https://restofworld.org/wp-content/projects/code/exercise/data/weather.js").then((res) => res.json())
-//     .then((data) => console.log(data))
-//   }
-// }
 
 function weatherJsonpCallback(data){
   const name = data.cities[9].geoloc.city
   const { temp, condition } = data.cities[9].current[0]
   const { weekly } = data.cities[9]
- console.log(weekly)
+
 document.querySelector(".city").innerText = `Weather in ${name}`
 //document.querySelector(".city").innerText = "Weather in" + name;
 document.querySelector(".temp").innerText = `${temp}°F`
@@ -35,12 +13,15 @@ document.querySelector(".condition").innerText = `${condition}`
  for(let i = 1; i <= 7; i++){
    let day = weekly[i];
  // Create a new element to hold each day's data in the DOM:
-   const dayContainer = document.createElement('div')
+   const dayContainer = document.getElementById("dailyForecast_contents")
    //set day
    const dayName = day.day
    const nameElem = document.createElement('div')
    nameElem.innerText = dayName
    dayContainer.appendChild(nameElem)
+   nameElem.classList.add("forecastDay")
+   let text = document.createTextNode("")
+   nameElem.appendChild(text)
    //set temp
    const dayHigh = day.high
    const highElem = document.createElement('div')
@@ -52,11 +33,50 @@ document.querySelector(".condition").innerText = `${condition}`
    descElem.innerText = `${dayDesc}`
    dayContainer.appendChild(descElem)
    //add to DOM
-   document.body.appendChild(dayContainer)
+  // document.body.appendChild(dayContainer)
+  dayContainer.appendChild(nameElem)
  }
 
+ //had a difficult time with the JSONP it was taking too much time to understand fully...i get all cities in console when click drop down, just not sure why they aren't displaying...I can't get functions inside the main function to work and not able to get the data outside the callback function 
+ let cities = data.cities
+ for(let i = 0; i < cities.length; i++){
+   let city = cities[i]
+   document.getElementById('generate').onclick = function() {
+    var values = cities;
+
+    var select = document.createElement("select");
+    select.name = "cities";
+    select.id = "cities"
+
+    for (const val of values)
+    {
+        var option = document.createElement("option");
+        option.value = val;
+        option.text = val.geoloc.city
+        select.appendChild(option);
+    }
+
+    var label = document.createElement("label");
+    label.innerHTML = "Choose your city: "
+    label.htmlFor = "cities";
+
+    document.getElementById("container").appendChild(label).appendChild(select);
+    }
+    let time =  cities[i].hourly[i]
+    let mode = document.getElementById("app");
+    console.log(time)
+    for(let t of time){
+        if(t.ampm === 'pm'){
+          mode.classList.add("darkmode").remove("lightmode");
+        }else{
+          mode.classList.remove("darkmode").add("lightmode");
+        }
+    }
+ }
 
 };
+
+
 
 var script = document.createElement('script')
 script.type = "text/javascript"
@@ -66,3 +86,6 @@ document.getElementsByTagName('head')[0].appendChild(script);
 // or document.head.appendChild(script) in modern browsers
 
 
+
+
+//axios.get("https://restofworld.org/wp-content/projects/code/exercise/data/weather.js").then()
